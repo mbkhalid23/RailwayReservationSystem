de3385ab12678a946310bec3f9317b0f04d0ae4b
mbkhalid23 <mbkhalid23@gmail.com>
2022-08-05 01:27:06 +0500

    Connect to SQL Server Database

---
+++ RailwayReservationSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailwayReservationSystem.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-6BSINQC;Database=RRSDB;Trusted_Connection=True;");
+        }
+
+       // public DbSet<Passenger> Passengers { get; set; }
+    }
+}
+ 
---
+++ RailwayReservationSystem/Migrations/20220804194723_Initial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RailwayReservationSystem.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
